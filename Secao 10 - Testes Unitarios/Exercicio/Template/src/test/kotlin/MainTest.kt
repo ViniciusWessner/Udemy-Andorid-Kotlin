ad9606c48494874e341c3b7d638b16255ff01315
vini.wessner@gmail.com <Capitao@caverna55>
2023-01-21 08:59:26 -0300

    Testando sistemas de portaria

--- Secao 10 - Testes Unitarios/Exercicio/Template/src/test/kotlin/MainTest.kt
+++ Secao 10 - Testes Unitarios/Exercicio/Template/src/test/kotlin/MainTest.kt
@@ -1,3 +1,24 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
 class MainTest {
 
+    @Test
+    @DisplayName("Testa os cenarios portaria")
+
+    fun testaPortaria(){
+        //verificando se idade é a correta
+        Assertions.assertEquals(portaria(0,"",""), "Negado.")
+        Assertions.assertEquals(portaria(20,"",""), "Negado.")
+        //Verificando o tipo de convite (passando um errado)
+        Assertions.assertEquals(portaria(20,"VIP",""), "Negado.")
+        // Verificando o código do convite com o "xt" e sem o "Xt"
+        Assertions.assertEquals(portaria(20,"comum","xt4545"), "Welcome.")
+        Assertions.assertEquals(portaria(20,"comum","4545"), "Negado.")
+        //Testando o tipo do convite premium e luxo com a validação do cod do convite XL
+        Assertions.assertEquals(portaria(20,"premium","xt5589"), "Negado.")
+        Assertions.assertEquals(portaria(20,"premium","xt5589"), "Negado.")
+        Assertions.assertEquals(portaria(20,"luxo","xl4545"), "Welcome.")
+    }
 }
